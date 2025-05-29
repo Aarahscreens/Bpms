171d624a72c501b256c6b781b138b52fdd584ad1
Aarahscreens <aarahscreens@gmail.com>
2025-05-30 00:15:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+ 
+     ?>
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <title>Aarah Screens | Home Page</title>
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+  </head>
+  <body id="home">
+
+<?php include_once('includes/header.php');?>
+
+<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
+<!--bootstrap working-->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- //bootstrap working-->
+<!-- disable body scroll which navbar is in active -->
+<script>
+$(function () {
+  $('.navbar-toggler').click(function () {
+    $('body').toggleClass('noscroll');
+  })
+});
+</script>
+<!-- disable body scroll which navbar is in active -->
+
+<div class="w3l-hero-headers-9">
+  <div class="css-slider">
+    <input id="slide-1" type="radio" name="slides" checked>
+    <section class="slide slide-one">
+      <div class="container">
+        <div class="banner-text">
+         <h4>Welcome to Aarah Screens</h4>
+          <h3><i>Safe from Mosquitoes,</i><br>
+         <i> Safe for Nature.</i></h3>
+
+            
+        </div>
+      </div>
+      
+    </section>
+    <input id="slide-2" type="radio" name="slides">
+    <section class="slide slide-two">
+      <div class="container">
+        <div class="banner-text">
+          <h4>Creative Meshes</h4>
+          <h3><i>Shield Your Home<br>
+          Sleep in Peace.</i></h3>
+          
+        </div>
+      </div>
+      <!-- <nav>
+        <label for="slide-2" class="prev">&#10094;</label>
+        <label for="slide-1" class="next">&#10095;</label>
+      </nav> -->
+    </section>
+    <header>
+      <label for="slide-1" id="slide-1"></label>
+      <label for="slide-2" id="slide-2"></label>
+    </header>
+  </div>
+</div> 
+<section class="w3l-call-to-action_9">
+    <div class="call-w3 ">
+        <div class="container">
+            <div class="grids">
+                    <div class="grids-content row">
+
+                        <div class="column col-lg-4 col-md-6 color-2 ">
+                            <div>
+                            <h4 class=" ">We Provide Best Products for your Choice</h4>
+                            <p class="para ">Protect Your Space. Embrace the Breeze.</p>
+                            <a href="about.php" class="action-button btn mt-md-4 mt-3">Read more</a>
+                        </div>
+                    </div>
+                        <div class="column col-lg-4 col-md-6 col-sm-6 back-image  ">
+                            <img src="assets/images/5.jpg" alt="product" class="img-responsive ">
+                        </div>
+                        <div class="column col-lg-4 col-md-6 col-sm-6 back-image2 ">
+                            <img src="assets/images/6.jpg" alt="product" class="img-responsive ">
+                          </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</section>
+<section class="w3l-teams-15">
+	<div class="team-single-main ">
+		<div class="container">
+		
+				<div class="column2 image-text">
+					<h3 class="team-head ">Welcome to a Bite-Free Home</h3>
+					<p class="para  text ">
+          Say goodbye to sleepless nights, buzzing pests, and harmful chemicals. Our premium mosquito mesh solutions create an invisible shield around your home—keeping mosquitoes out while letting fresh air and light in.</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="w3l-specification-6">
+    <div class="specification-layout ">
+        <div class="container">
+            <div class=" row">
+                <div class="col-lg-6 back-image">
+                    <img src="assets/images/b1.jpg" alt="product" class="img-responsive ">
+                </div>
+                <div class="col-lg-6 about-right-faq align-self">
+                    <h3 class="title-big"> Recommended Best and Quality Products</a></h3>
+                    <p class="mt-3 para"> To make every species safer and more comfortable by offering eco-friendly, durable, and easy to install mosquito mesh products.</p>
+                        <div class="hair-cut">
+                            <div >
+                    <ul class="w3l-right-book">
+                        <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Insect Protection</a></li>
+                        <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Breathability</a></li>
+                        <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Visibility</a></li>
+                        <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Durability</a></li>
+                        <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Eco-Friendly</a></li>
+                    </ul>
+                </div>
+                    <div  class="image-right">
+                        <ul class="w3l-right-book">
+                            <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Custom fit</a></li>
+                            <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Affordable</a></li>
+                            <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Reusable</a></li>
+                            <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Premium Quality</a></li>
+                            <li><span class="fa fa-check" aria-hidden="true"></span><a href="about.html">Safe for Chidren and Pets</a></li>
+                        </ul>
+                </div>
+            </div>
+        </div>
+</section>
+<?php include_once('includes/footer.php');?>
+<!-- move top -->
+<button onclick="topFunction()" id="movetop" title="Go to top">
+	<span class="fa fa-long-arrow-up"></span>
+</button>
+<script>
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("movetop").style.display = "block";
+		} else {
+			document.getElementById("movetop").style.display = "none";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+</script>
+<!-- /move top -->
+</body>
+
+</html>
